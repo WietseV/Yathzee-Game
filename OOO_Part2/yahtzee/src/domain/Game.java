c91cee2aac8b6b3eb146eb6ceea331d6f3881546
Wiietse Vandeput <wietse.vandeput@hotmail.com>
2018-05-08 15:53:47 +0200

    bezig met implementatie

---
+++ OOO_Part2/yahtzee/src/domain/Game.java
@@ -0,0 +1,12 @@
+package domain;
+
+public class Game {
+	
+	PlayerGroup playerGroup = new PlayerGroup();
+
+	public void regPlayer(String name){
+		playerGroup.add();
+	}
+}
+
+
---
+++ OOO_Part2/yahtzee/src/domain/PlayerGroup.java
@@ -0,0 +1,12 @@
+package domain;
+
+import java.util.*;
+
+public class PlayerGroup {
+	
+	private List<Player> playerList = new ArrayList<Player>();
+
+	public void add(Player player) {
+		
+	}
+}
