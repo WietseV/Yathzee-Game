22c494fb5d657a672b6d0e4f898ff42181ce5f8d
Wiietse Vandeput <wietse.vandeput@hotmail.com>
2018-05-08 15:56:31 +0200

    domainmodel finished

--- OOO_Part2/yahtzee/src/domain/Game.java
+++ OOO_Part2/yahtzee/src/domain/Game.java
@@ -5,7 +5,8 @@ public class Game {
 	PlayerGroup playerGroup = new PlayerGroup();
 
 	public void regPlayer(String name){
-		playerGroup.add();
+		Player player = new Player(name);
+		playerGroup.add(player);
 	}
 }
 
--- OOO_Part2/yahtzee/src/domain/PlayerGroup.java
+++ OOO_Part2/yahtzee/src/domain/PlayerGroup.java
@@ -7,6 +7,10 @@ public class PlayerGroup {
 	private List<Player> playerList = new ArrayList<Player>();
 
 	public void add(Player player) {
-		
+		playerList.add(player);
+	}
+	
+	public List<Player> getList() {
+		return playerList;
 	}
 }
