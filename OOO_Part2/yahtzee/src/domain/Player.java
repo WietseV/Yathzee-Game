cc98ba264932bd08f4cd085c169de678987b9ad1
Gleb <glebshalabotov@student.ucll.be>
2018-05-08 15:52:51 +0200

    speler naar player veranderd

---
+++ OOO_Part2/yahtzee/src/domain/Player.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Player {
+
+	private String name;
+	public Player(String name) {
+		setName(name);
+	}
+	
+	private void setName(String name) {
+		this.name = name;
+	}
+}
